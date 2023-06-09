a81823d26724eb51694f9a8f8fc1bd04e6283bc1
Ivo Peterka <peterkai@gmail.com>
2023-06-09 10:03:08 +0100

    Initial project setup

---
+++ src/main/java/io/github/peterkaivo/muzmuz/MuzMuzApplication.java
@@ -0,0 +1,13 @@
+package io.github.peterkaivo.muzmuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzMuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzMuzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/peterkaivo/muzmuz/MuzMuzApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.peterkaivo.muzmuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzMuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
